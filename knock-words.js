5d0cfd2c1a7f4f56ff2156260d3dce080c5ac2ae
Daniel Bulla <dabuar98@gmail.com>
2024-05-02 20:36:25 -0500

    Complete printMessage fxn

--- knock-words.js
+++ knock-words.js
@@ -8,7 +8,7 @@ const messages = [
     {name: "Luke", pun: "Luke through the peephole and find out!"},
     {name: "Justin", pun: "Justin time for dinner!"},
     {name: "Art", pun: "R2D2."},
-    {name: "Dewey", pun: "Do we have to keep telling knock-knock jokes?"}
+    {name: "Dewey", pun: "Dewey have to keep telling knock-knock jokes?"}
 ];
 
 // Gets a random number to choose the "joke"
@@ -19,16 +19,11 @@ function generateRandomNumber() {
 
 function printMessage()
 {
-    // console.log("Knock, knock?\nWho's there");
-    // for (let i = 0; i < messages.length; i++)
-    // {
-    //     console.log(
-    //         `Knock, knock.\nWho's there?\n${messages[i].object}.\n${messages[i].object} who?\n${messages[i].phrase}\n * laughs in the background *`
-    //     )
-    //     // messages[i].object, messages[i].phrase);
-    // }
-    console.log(messages.length)
-}
+    let n = generateRandomNumber();
+    console.log(
+        `Knock, knock.\nWho's there?\n${messages[n].name}.\n${messages[n].name} who?\n${messages[n].pun}\n * laughs in the background *`
+    )
+};
 
 printMessage();
 
